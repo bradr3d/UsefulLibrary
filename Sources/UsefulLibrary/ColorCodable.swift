03708ded595096b50e0de8433cb25797f82b2c95
Brad <bradleyethan428@gmail.com>
2025-02-11 12:46:44 -0500

    fix color codable conformance

--- Sources/UsefulLibrary/ColorCodable.swift
+++ Sources/UsefulLibrary/ColorCodable.swift
@@ -53,28 +53,30 @@ fileprivate extension Color {
 @available(iOS 13.0, *)
 extension Color: Codable {
     enum CodingKeys: String, CodingKey {
-        case red, green, blue
+        case red, green, blue, alpha
     }
-    
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let r = try container.decode(Double.self, forKey: .red)
         let g = try container.decode(Double.self, forKey: .green)
         let b = try container.decode(Double.self, forKey: .blue)
+        let a = try container.decode(Double.self, forKey: .alpha) // Added alpha
         
-        self.init(red: r, green: g, blue: b)
+        self.init(red: r, green: g, blue: b, opacity: a)
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         guard let colorComponents = self.colorComponents else {
             return
         }
         
         var container = encoder.container(keyedBy: CodingKeys.self)
-        
+
         try container.encode(colorComponents.red, forKey: .red)
         try container.encode(colorComponents.green, forKey: .green)
         try container.encode(colorComponents.blue, forKey: .blue)
-        
+        try container.encode(colorComponents.alpha, forKey: .alpha) // Added alpha
     }
 }
+
